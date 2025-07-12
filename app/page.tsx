4798ec1dddb0515647c43ca2669ec6d0d9d730a0
umut <umutcerk@gmail.com>
2025-07-12 15:38:26 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Mentra</h1>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+export default Page
